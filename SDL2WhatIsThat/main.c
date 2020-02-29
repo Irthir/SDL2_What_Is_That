71fafa7a3149226b4c89dbf1974a4103b4c9bb2d
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-02-29 14:34:38 +0100

    Question 6) Initialisation de la SDL2.

--- SDL2WhatIsThat/main.c
+++ SDL2WhatIsThat/main.c
@@ -1,41 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "SDL2/SDL.h"
+#include "SDL2/SDL.h" //Inclusion de la bibliothèque SDL
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])//modification du main pour y ajouter un compteur d'arguments et un tableau de chaines.
 {
+    //Initialisation de trois pointeurs sur référence NULL pour pouvoir les manipuler par la suite.
     SDL_Window *pWindow=NULL; //Pointeur typedef SDL_Window possède la référence d'une window(win32)
     SDL_Renderer *pRenderer=NULL; //Pointeur typedef SDL_Renderer possède la référence du rendu encapsulé dans window.
+    SDL_Texture *pTexture=NULL; //Pointeur typedef SDL_Texture possède la référence de l'objet image, matériel ou texture qui sera blitté dans le rendu.
+    //La SDL manipule des types.
 
-    if(SDL_Init(SDL_INIT_EVERYTHING)>=0)
+    //Initialisation de la SDL.
+    if(SDL_Init(SDL_INIT_EVERYTHING)>=0)//Si il n'y a pas d'erreur alors la SDL fonctionne.
     {
-        //Si l'initialisation marche on crée notre fenêtre.
-        //On crée le pointeur sur la référence de la fenêtre Win32.
-        pWindow=SDL_CreateWindow("SDL2WhatIsThat",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,1024,780,SDL_WINDOW_SHOWN);
-        //https://wiki.libsdl.org/SDL_CreateWindow
-
-        //En cas de succès de création de la fenêtre, créer un rendu.
-        //Si la fenêtre est créée.
-        if(pWindow!=NULL)
-        {
-            //Création du rendu encapsulé dans la fenêtre pointée par pWindow.
-            pRenderer=SDL_CreateRenderer(pWindow,-1,SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
-        }
-
-        //Traitement de l'affichage.
-        //Set de la couleur du traitement.
-        SDL_SetRenderDrawColor(pRenderer, 0, 0, 0, SDL_ALPHA_OPAQUE);
-        //Effaceemnt de l'état precedent du renderer.
-        SDL_RenderClear(pRenderer);
-        //Affiche l'état du nouveau rendu.
-        SDL_RenderPresent(pRenderer);
-        //Fin du traitement de l'affichage.
-
-        //Synchronisation et rafraichissement d'affichage des frames à l'écran.
-        SDL_Delay(4000);
+        printf("La SDL est initialisee !\n");
     }
-    SDL_DestroyRenderer(pRenderer);
-    SDL_DestroyWindow(pWindow);
-    SDL_Quit();
+    SDL_Quit();//Fermeture de la SDL.
+    printf("Fin de l'utilisation de la SDL.\n");
+
+    printf("Fin du programme.\n");
     return 0;
 }
